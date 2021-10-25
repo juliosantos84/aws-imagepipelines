e18e8b8948ddb8bdca18f9ea9f9bc22d87574876
Julio Santos <julio.santos@venmo.com>
2021-10-25 00:33:36 -0400

    Tweak volume mounts. Fix the stupid test, I think.

--- src/main/resources/assets/etherythingbiig/scripts/general/mount-volume.sh
+++ src/main/resources/assets/etherythingbiig/scripts/general/mount-volume.sh
@@ -1,15 +1,16 @@
 #! /bin/bash
 
 VOLUME_MOUNT_PATH=$(cat /home/ubuntu/volume-mount-path)
-# VOLUME_MOUNT_PATH_OWNER=$(cat /home/ubuntu/volume-mount-path-owner)
 
 # Mount the volume
 echo "Mounting volume..." && sudo mkdir -p ${VOLUME_MOUNT_PATH} \
 && sudo mount /dev/nvme1n1 ${VOLUME_MOUNT_PATH}
 
 echo "Creating subdirectories..." \
-&& sudo mkdir -p ${VOLUME_MOUNT_PATH}/{lighthouse,goethereum}
+&& sudo mkdir -p ${VOLUME_MOUNT_PATH}/goethereum \
+&& sudo mkdir -p ${VOLUME_MOUNT_PATH}/lighthouse/validators
 
 echo "Setting ownership..." \
 && sudo chown -R goeth:goeth ${VOLUME_MOUNT_PATH}/goethereum \
-&& sudo chown -R lighthousebeacon:lighthousebeacon ${VOLUME_MOUNT_PATH}/lighthouse
+&& sudo chown -R lighthousebeacon:lighthousebeacon ${VOLUME_MOUNT_PATH}/lighthouse \
+&& sudo chown -R lighthousevalidator:lighthousevalidator ${VOLUME_MOUNT_PATH}/lighthouse/validators
