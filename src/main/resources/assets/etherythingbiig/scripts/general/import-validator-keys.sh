6502f56ddec11ed0bc39f52fe495fc2df9590ca4
Julio Santos <julio.santos@venmo.com>
2021-10-25 14:22:01 -0400

    pipeline script, fix validator script perms and bump versions.

--- src/main/resources/assets/etherythingbiig/scripts/general/import-validator-keys.sh
+++ src/main/resources/assets/etherythingbiig/scripts/general/import-validator-keys.sh
@@ -4,8 +4,6 @@ set -x
 
 set -e
 
-sudo mkdir -p /var/lib/lighthouse/validators && sudo chown ubuntu:ubuntu /var/lib/lighthouse/validators
-
 mv $HOME/validator_keys/import.json $HOME/validator_keys/keystore-m_12381_3600_0_0_0-1632692812.json
 
-lighthouse --network pyrmont account validator import --directory $HOME/validator_keys --datadir /var/lib/lighthouse
+lighthouse --network pyrmont account validator import --directory $HOME/validator_keys --datadir /var/lib/chaindata/lighthouse
