abb2228b7e15b73f49d9b623e4c74ca5450ac6b4
Julio Santos <julio.santos@venmo.com>
2021-10-29 15:44:16 -0400

    Add debug and exit on error to deploy script.

--- bin/deploy-pipeline.sh
+++ bin/deploy-pipeline.sh
@@ -1,15 +1,20 @@
 #! /bin/bash
 
+set -e
+
+set -x
+
 RECIPE_VERSION=""
 COMPONENT_VERSION=""
 
 if [ ! -z "${1}" ]; then
     RECIPE_VERSION="-c everythingbiig-aws-imagepipelines/etherythingbiig:recipeVersion=${1}"
     COMPONENT_VERSION="-c everythingbiig-aws-imagepipelines/etherythingbiig:componentVersion=${1}"
+    echo -e "Overriding versions with\n\t${RECIPE_VERSION}\n\t${COMPONENT_VERSION}"
 fi
 
 VERSION=${1:-"from cdk.json"}
 
-echo "Deploying version ${VERSION} with\n ${RECIPE_VERSION} ${COMPONENT_VERSION}"
+echo "Deploying version ${VERSION}"
 cdk deploy etherythingbiigImagePipeline \
 --require-approval never ${RECIPE_VERSION} ${COMPONENT_VERSION}
