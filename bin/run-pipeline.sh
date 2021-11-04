00306880c5e309e6559d1bce532ce0ed61669007
Julio Santos <julio.santos@venmo.com>
2021-11-03 21:30:36 -0400

    Fix geth and ligthhouse install tests.  Fix deploy-pipeline args.

--- bin/run-pipeline.sh
+++ bin/run-pipeline.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
 
+# export CDK_DEPLOY_ACCOUNT=""
+# export CDK_DEPLOY_REGION=""
+# export CDK_DEPLOY_PROFILE_FLAG="--profile your profile"
+# export CDK_DEPLOY_EXTRA_CONTEXT="-c everythingbiig-aws-imagepipelines/etherythingbiig:distributionRegions.0=us-east-1"
+
 PIPELINE_NAME="etherythingbiigImagepipeline"
 PIPELINE_ARN=$(aws imagebuilder list-image-pipelines \
---filters "name=name,values=${PIPELINE_NAME}" \
+--filters "name=name,values=${PIPELINE_NAME}" ${CDK_DEPLOY_PROFILE_FLAG} \
 | jq -r '.imagePipelineList[0].arn')
 
-aws imagebuilder start-image-pipeline-execution --image-pipeline-arn $PIPELINE_ARN
+aws imagebuilder start-image-pipeline-execution --image-pipeline-arn $PIPELINE_ARN ${CDK_DEPLOY_PROFILE_FLAG} ${CDK_DEPLOY_EXTRA_CONTEXT}
