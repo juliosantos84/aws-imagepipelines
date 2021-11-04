ca82d74926733e37fe37b07012239b8a759b14f5
Julio Santos <julio.santos@venmo.com>
2021-11-04 09:24:13 -0400

    Add var-lib-chaindata-directory-creator.service to manage dir permissions.

--- src/main/resources/assets/etherythingbiig/scripts/general/create-directories.sh
+++ src/main/resources/assets/etherythingbiig/scripts/general/create-directories.sh
@@ -2,10 +2,6 @@
 
 VOLUME_MOUNT_PATH=$(cat /home/ubuntu/volume-mount-path)
 
-# Mount the volume
-echo "Mounting volume..." && sudo mkdir -p ${VOLUME_MOUNT_PATH} \
-&& sudo mount /dev/nvme1n1 ${VOLUME_MOUNT_PATH}
-
 echo "Creating subdirectories..." \
 && sudo mkdir -p ${VOLUME_MOUNT_PATH}/goethereum \
 && sudo mkdir -p ${VOLUME_MOUNT_PATH}/lighthouse/validators
