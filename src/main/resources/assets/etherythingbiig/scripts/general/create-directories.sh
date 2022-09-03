71ac646c5f9781304080c12f709798e059356c6a
juliosantos84 <julio@getflex.com>
2022-09-03 12:40:06 -0400

    Move jwtsecret to /var/lib/chaindata/beaconshared/jwtsecret.  Fix permissions.  Remove variable for execution endpoint.

--- src/main/resources/assets/etherythingbiig/scripts/general/create-directories.sh
+++ src/main/resources/assets/etherythingbiig/scripts/general/create-directories.sh
@@ -4,9 +4,11 @@ VOLUME_MOUNT_PATH=$(cat /home/ubuntu/volume-mount-path)
 
 echo "Creating subdirectories..." \
 && sudo mkdir -p ${VOLUME_MOUNT_PATH}/goethereum \
-&& sudo mkdir -p ${VOLUME_MOUNT_PATH}/lighthouse/validators
+&& sudo mkdir -p ${VOLUME_MOUNT_PATH}/lighthouse/validators \ 
+&& sudo mkdir -p ${VOLUME_MOUNT_PATH}/beaconshared
 
 echo "Setting ownership..." \
 && sudo chown -R goeth:goeth ${VOLUME_MOUNT_PATH}/goethereum \
 && sudo chown -R lighthousebeacon:lighthousebeacon ${VOLUME_MOUNT_PATH}/lighthouse \
-&& sudo chown -R lighthousevalidator:lighthousevalidator ${VOLUME_MOUNT_PATH}/lighthouse/validators
+&& sudo chown -R lighthousevalidator:lighthousevalidator ${VOLUME_MOUNT_PATH}/lighthouse/validators \
+&& sudo chown -R :beaconshared ${VOLUME_MOUNT_PATH}/beaconshared
